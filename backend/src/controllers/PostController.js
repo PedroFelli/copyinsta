59198ec2f817d87b4a515ff83a4ef1c4defed33a
PedroFelli <pedrofellipeunai@gmail.com>
2019-06-13 16:42:12 -0300

    backend
    terminando backend

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -13,13 +13,19 @@ module.exports = {
     async store(req, res) {
         const { author, place, description, hashtags} = req.body;
         const {filename: image } = req.file;
+
+        const [name] = image.split('.');
+        const fileName = `${name}.jpg`;
+
  
         await sharp(req.file.path)
         .resize(500)
         .jpeg({ quality: 70})
         .toFile(
-            path.resolve(req.file.destination, 'resized', image)
+            path.resolve(req.file.destination, 'resized', fileName)
         )
+
+        fs.unlinkSync(req.file.path);
 
 
         const post = await Post.create({
@@ -27,8 +33,10 @@ module.exports = {
             place,
             description,
             hashtags,
-            image
+            image : fileName,
         });
+
+        req.io.emit('post', post);
 
         return res.json(post);
     }
